e7d0508be8ef9d0488d05443dd847458816a5303
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-04-03 11:51:16 -0500

    adding random number function for all crystal vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,18 +3,34 @@ $(document).ready(function() {
 
 var number = "";
 
-function randomIntFromInterval(min,max)
+function randomMatchNumber(min,max)
 {
     return Math.floor(Math.random()*(120-19+1)+19);
 }
 
-number = randomIntFromInterval() + number;
+number = randomMatchNumber() + number;
 
 $("#number").append("<br>" + number);
 
 console.log(number);
 
 
+var crystalOneNumber ="";
+var crystalTwoNumber ="";
+var crystalThreeNumber ="";
+var crystalFourNumber ="";
+
+function randomCrystalNumber(min,max)
+{
+    return Math.floor(Math.random()*(12-1+1)+1);
+}
+
+crystalOneNumber = randomCrystalNumber() + crystalOneNumber;
+crystalTwoNumber = randomCrystalNumber() + crystalTwoNumber;
+crystalThreeNumber = randomCrystalNumber() + crystalThreeNumber;
+crystalFourNumber = randomCrystalNumber() + crystalFourNumber;
+
+console.log(crystalOneNumber);
 
 
 });
